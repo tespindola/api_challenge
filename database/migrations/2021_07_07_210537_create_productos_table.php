27b7bdfd9d7f7b5aea7f866a1ff14d4a7a23275b
Tomas Ezequiel Espindola <tespindola@leverbox.com>
2021-07-07 18:09:23 -0300

    Primer commit

---
+++ database/migrations/2021_07_07_210537_create_productos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(){
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 255);
+            $table->mediumText('descripcion');
+            $table->float('cantidad');
+            $table->float('precio_compra');
+            $table->float('total_compra');
+            $table->float('precio_venta');
+            $table->float('total_venta');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(){
+        Schema::dropIfExists('productos');
+    }
+}
